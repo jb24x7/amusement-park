4fecb4e890c46bce5c3e7d43ff9ac449850b09d8
James Provance <jb24x7@gmail.com>
2023-01-11 12:59:43 -0800

    alered truly falsly elements

--- js/scripts.js
+++ js/scripts.js
@@ -15,22 +15,22 @@ window.onload = function() {
     const age = parseInt(document.querySelector("input#age").value);
     const height = parseInt(document.querySelector("input#height").value);
 
-  if (age && height) {
-    if (age >= 12 && height >= 60) {
-      document.getElementById("swings").removeAttribute("class");
-      document.getElementById("coaster").removeAttribute("class");
-      document.getElementById("tower").removeAttribute("class");
-    } else if (age >= 12 && height >= 48) {
-      document.getElementById("swings").removeAttribute("class");
-      document.getElementById("coaster").removeAttribute("class");
-    }else if (age >= 6 || height >=32) {
-      document.getElementById("swings").removeAttribute("class");
-      // Finally, we have our "catch-all" for when a user is less than 6 years old.
-    } else {
-      document.getElementById("sorry").removeAttribute("class");
+    if (!age || !height) {
+      document.getElementById("error-message").removeAttribute("class");
+    } else {  
+      if (age >= 12 && height >= 60) {
+        document.getElementById("swings").removeAttribute("class");
+        document.getElementById("coaster").removeAttribute("class");
+        document.getElementById("tower").removeAttribute("class");
+      } else if (age >= 12 && height >= 48) {
+        document.getElementById("swings").removeAttribute("class");
+        document.getElementById("coaster").removeAttribute("class");
+      }else if (age >= 6 || height >=32) {
+        document.getElementById("swings").removeAttribute("class");
+        // Finally, we have our "catch-all" for when a user is less than 6 years old.
+      } else {
+        document.getElementById("sorry").removeAttribute("class");
+      }
     }
-  } else {
-    document.getElementById("error-message").removeAttribute("class");
-  }
   };
 };
